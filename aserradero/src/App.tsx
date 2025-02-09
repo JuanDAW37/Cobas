69e0b9c4ea1c2c5b7750759c50d9bfb48802f876
JuanDAW37 <jymtr1968@gmail.com>
2025-02-09 10:41:47 +0100

    comienzo de backend

--- aserradero/src/App.tsx
+++ aserradero/src/App.tsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // -> hook de estado de React relaciona un variable para que se pueda usar en el renderizado
+  // -> useState(0) es el valor inicial de la variable count
+  // -> setCount es la función que se usa para cambiar el valor de la variable count
+  // -> count es la variable que se usa en el renderizado
+  const [count, setCount] = useState(0) 
+  const [name, setName] = useState('Juan')
+
+  const countMore = () => {
+    setCount((count) => count + 1) 
+  }  
+
+  const changeName = () => {
+    setName('Pedro')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>      
+        <Button label={`La cuenta es ${count}`} parentMethod = {countMore}/>
+        <p>{`El nombre es ${name}`}</p>            
+        <Button label={`Cambiar a ${name}`} parentMethod = {changeName}/>
     </>
   )
 }
